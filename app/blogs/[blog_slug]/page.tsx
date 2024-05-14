e43ac857f85cc440e2d3f711b2a9009446901e97
alamin-nifty <alaminkhan@niftyitsolution.com>
2024-05-14 12:36:47 +0600

    fix:api route fixed for caching

--- app/blogs/[blog_slug]/page.tsx
+++ app/blogs/[blog_slug]/page.tsx
@@ -1,5 +1,5 @@
 const getBlogBySlug = async (slug: string) => {
-  const res = await fetch(`http://localhost:3000/api/blogs/${slug}`);
+  const res = await fetch(`/api/blogs/${slug}`);
   const data = await res.json();
   console.log(data);
   return data;
